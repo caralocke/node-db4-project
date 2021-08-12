8f771c31c6f0ff359993934754f1efea29b3d542
Cara M Locke <81659057+caralocke@users.noreply.github.com>
2021-08-12 17:40:19 -0400

    Completed writing code needed to create the steps table. Also fixed an issue where I forgot to drop existing tables upon migrating down.

--- data/migrations/20210812151631_build-tables.js
+++ data/migrations/20210812151631_build-tables.js
@@ -10,9 +10,26 @@ exports.up = function(knex) {
       table.string('recipe_name', 128).unique().notNullable()
       table.timestamp('created_at').defaultTo(knex.fn.now())
   })
+  .createTable('steps', (table) => {
+      table.increments('steps_id')
+      table.integer('step_number')
+        .unsigned()
+        .notNullable()
+    table.string('instructions')
+        .notNullable()
+    table.integer('recipe_id')
+        .unsigned()
+        .notNullable()
+        .references('recipe_id')
+        .inTable('recipes')
+        .onDelete('RESTRICT')
+        .onUpdate('RESTRICT')
+  })
 };
 
 exports.down = function(knex) {
   return knex.schema
+  .dropTableIfExists('steps')
+  .dropTableIfExists('recipes')
   .dropTableIfExists('ingredients')
 };
