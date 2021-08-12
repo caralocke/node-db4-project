a0ba2e85bd646d588484a3507cc8126503120eef
Cara M Locke <81659057+caralocke@users.noreply.github.com>
2021-08-12 17:26:40 -0400

    Completed writing code needed to create the recipes table.

--- data/migrations/20210812151631_build-tables.js
+++ data/migrations/20210812151631_build-tables.js
@@ -3,8 +3,12 @@ exports.up = function(knex) {
   return knex.schema
   .createTable('ingredients', (table) => {
       table.increments('ingredients_id')
-      table.string('recipe_name', 256).unique().notNullable()
-      table.timestamp('created_at')
+      table.string('ingredient_name', 128).unique().notNullable()
+  })
+  .createTable('recipes', (table) => {
+      table.increments('recipes_id')
+      table.string('recipe_name', 128).unique().notNullable()
+      table.timestamp('created_at').defaultTo(knex.fn.now())
   })
 };
 
