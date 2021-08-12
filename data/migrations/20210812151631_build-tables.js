21fdefd39cd1438b631e841bb8ae8116592b8866
Cara M Locke <81659057+caralocke@users.noreply.github.com>
2021-08-12 17:11:23 -0400

    Completed creating the server files and creating the bare-bones of what I believe I'll need for the project. Also verified server can run and that migrating up creates a db3 file for me.

--- data/migrations/20210812151631_build-tables.js
+++ data/migrations/20210812151631_build-tables.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema
+  .createTable('ingredients', (table) => {
+      table.increments('ingredients_id')
+      table.string('recipe_name', 256).unique().notNullable()
+      table.timestamp('created_at')
+  })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+  .dropTableIfExists('ingredients')
 };
